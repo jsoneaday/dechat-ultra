5a1252e957ca8fd4cd5567d73736b5493c981f48
David Choi <dharric@live.com>
2023-02-16 21:24:59 -0500

    fix tailwind not working issues by adding postcss

--- packages/nativeclient/src/components/Layout.tsx
+++ packages/nativeclient/src/components/Layout.tsx
@@ -2,8 +2,12 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <main>
-      <Outlet />
-    </main>
+    <div className="grid gap-2 grid-cols-4 grid-rows-1">
+      <div>Menu</div>
+      <div className="col-span-2">
+        <Outlet />
+      </div>
+      <div>Pops</div>
+    </div>
   );
 }
