a8271e77b43d1fd1f6455129e2b787921a00dfdf
David Choi <dharric@live.com>
2023-02-18 22:03:02 -0500

    updated test for Nav

--- packages/nativeclient/src/components/common/Nav.spec.tsx
+++ packages/nativeclient/src/components/common/Nav.spec.tsx
@@ -2,9 +2,19 @@ import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../../App";
 import router from "./Routing";
+import {
+  createMemoryRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Nav from "./Nav";
 
 describe("Test nav menus", () => {
   it("All nav menus render successfully", async () => {
+    const router = createMemoryRouter(
+      createRoutesFromElements(<Route path="/" element={<Nav />} />)
+    );
+
     render(<App router={router} />);
 
     expect(screen.getByText("Home")).toBeInTheDocument();
